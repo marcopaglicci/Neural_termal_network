3fe6921c48553ac9ca29a5b7d1e829c860bb191b
Marco <marcopaglicci22@gmail.com>
2025-04-02 12:30:45 +0200

    added script for model validation

---
+++ model_validation.py
@@ -0,0 +1,24 @@
+
+from ultralytics import YOLO
+
+
+# Inserire il nome del modello su cui effettuare il test finale
+model_name = "custom_03"
+
+path_to_bestweight = "/testing/"+model_name+"/weights/best.pt"
+
+validation_name = model_name + "_validation"
+folder = "test"
+
+# Carica il modello con i pesi
+model = YOLO(path_to_bestweight)
+
+# Esegui inferenza sul video
+metrics = model.val(
+    data = "test_dataset.yaml",
+    name=validation_name,
+    project = folder,
+    plot = True,  # Salva i grafici delle metriche
+)
+
+print(f"▶️Test completato. Risultati salvati in {folder}/{validation_name}.")
